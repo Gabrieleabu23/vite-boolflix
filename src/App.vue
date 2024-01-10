5ece5da651cf9ed793a1859bf5f096c16cb175bb
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-01-10 23:35:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>asdasd</h1>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>
